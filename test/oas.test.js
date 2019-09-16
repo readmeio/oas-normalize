6cd19b26989424e748645e9c0290c5b51fed6c91
Jon Ursenbach <jon@ursenba.ch>
2019-09-16 16:26:26 -0700

    Adding some more tests for parsing URLs.

--- test/oas.test.js
+++ test/oas.test.js
@@ -3,8 +3,20 @@ const OAS = require('../oas');
 
 describe('#validate', () => {
   describe('Swagger', () => {
+    it('should validate a URL hosting JSON as expected', done => {
+      const o = new OAS('http://petstore.swagger.io/v2/swagger.json');
+      o.validate((err, definition) => {
+        expect(definition.openapi).toBe('3.0.0');
+        expect(Object.keys(definition.paths).length).toBe(14);
+        expect(Object.keys(definition.components.requestBodies).length).toBe(2);
+        expect(Object.keys(definition.components.securitySchemes).length).toBe(2);
+        expect(Object.keys(definition.components.schemas).length).toBe(6);
+        done();
+      }, true);
+    });
+
     it('should validate a JSON path as expected', done => {
-      const contents = path.join(__dirname, 'fixtures', 'swagger', 'petstore.json'); // .toString();
+      const contents = path.join(__dirname, 'fixtures', 'swagger', 'petstore.json');
       const o = new OAS(contents, { enablePaths: true });
 
       o.validate((err, definition) => {
@@ -19,6 +31,16 @@ describe('#validate', () => {
   });
 
   describe('OpenAPI', () => {
+    it('shoudl validate a URL hosting YAML as expected', done => {
+      const o = new OAS('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore-expanded.yaml');
+      o.validate((err, definition) => {
+        expect(definition.openapi).toBe('3.0.0');
+        expect(Object.keys(definition.paths).length).toBe(2);
+        expect(Object.keys(definition.components.schemas).length).toBe(3);
+        done();
+      }, true);
+    });
+
     it('should validate a YAML path as expected', done => {
       const contents = path.join(__dirname, 'fixtures', 'openapi', 'petstore.yaml');
       const o = new OAS(contents, { enablePaths: true });
@@ -45,18 +67,3 @@ describe('#validate', () => {
     });
   });
 });
-
-
-/* var o = new OAS('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/yaml/petstore-simple.yaml');
-var o = new OAS('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore-expanded.yaml');
-var o = new OAS('http://petstore.swagger.io/v2/swagger.json');
-
-const path = require('path');
-var contents = path.join(__dirname, 'examples', 'err-v3-petstore-ref.yaml'); //.toString();
-var contents = path.join(__dirname, 'examples', 'err-v2-petstore.json'); //.toString();
-
-var o = new OAS(contents, { enablePaths: true });
-
-o.validate(function(err, version) {
-  console.log(err, version);
-}, true); */
