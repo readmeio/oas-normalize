c8c3da1bdebd64fa76b6678f3c6b6453efca78fc
Jon Ursenbach <jon@ursenba.ch>
2023-05-01 12:29:34 -0700

    Revert "chore: removing node-fetch for isomorphic-fetch"
    This reverts commit 006ba292afa0dbf571d157c01e4a3cf9b529c342.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 import openapiParser from '@readme/openapi-parser';
 import postmanToOpenAPI from '@readme/postman-to-openapi';
-import 'isomorphic-fetch';
+import fetch from 'node-fetch';
 
 import converter from 'swagger2openapi';
 
