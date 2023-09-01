d115d700f2736059a510033f8e37c9f6ad269b2e
Jon Ursenbach <erunion@users.noreply.github.com>
2023-09-01 15:35:19 -0700

    feat: support for esm builds (#290)
    * feat: support for esm builds
    
    * fix: tsconfig

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -269,6 +269,7 @@ describe('#validate', () => {
 
   // Skipping because the `chalk` dependency of `better-ajv-errors` within `openapi-parser` has
   // issues in CI. Test works fine locally though!
+  // eslint-disable-next-line vitest/no-disabled-tests
   it.skip('should colorize errors when `opts.colorizeErrors` is present', async () => {
     const o = new OASNormalize(require.resolve('./__fixtures__/invalid/openapi-3.1.json'), {
       colorizeErrors: true,
